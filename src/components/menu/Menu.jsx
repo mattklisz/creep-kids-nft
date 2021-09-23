feff3fc4e6fb86ca1427c8fd1ad412ac2479f587
Matt Klisz <mattklisz@gmail.com>
2021-09-23 10:34:08 -0600

    more impl

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { isMobile } from 'react-device-detect'
 import './Menu.css'
 
 export const Menu = () => {
-  return (
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openHandler = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return isMobile ? (
+    <div className="menu-container-mobile" onClick={openHandler}>
+      <div className={`menu-label${isOpen ? '-open' : ''}`}>
+        {'MENU'}
+      </div>
+      {
+        isOpen ?
+        <div>
+          <a href="#section_buy" className="menu-link">BUY</a>
+          <a href="#section_how_to" className="menu-link">HOW TO</a>
+          <a href="#section_roadmap" className="menu-link">ROADMAP</a>
+          <a href="#section_faq" className="menu-link">FAQ</a>
+          <a href="#section_about" className="menu-link">ABOUT</a>
+          <a href="#section_help" className="menu-link">HELP</a>
+        </div> : null
+      }
+    </div>
+  )
+  : (
     <div className="menu-container">
       <a href="#section_buy">BUY</a>
       <a href="#section_how_to">HOW TO</a>
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isMobile } from 'react-device-detect'
 import {
   Button,
   Menu
@@ -6,51 +7,56 @@ import {
 import './Home.css'
 
 export const Home = () => {
+
+  const getContainerClass = () => isMobile ? '' : 'grid-container';
+
   return (
-    <div className="home-container">
-      <div className="logo">
-        <img src={"./assets/images/creep_kids_logo-red.gif"} alt="" height="200" width="440" />
-      </div>
-      <div className="menu">
-        <Menu />
-      </div>
-      <div className="teaser">
-        <img src={"./assets/images/NFT-TEASE-V1.gif"} alt="" height="320" width="320" />
-      </div>
-      <div id="section_buy" className="section mint">
-        <Button text={'mint'} />
-      </div>
-      <div className="copy">
-        {'Creep Kids are immortal souls minted in real time, programmatically on the Ethereum blockchain. There are 1000 unique possibilities, no two are alike. They are NFT\'s that can be traded on Opensea.'}
-      </div>
-      <div className="gallery">
-        Gallery TBD
-      </div>
-      <div className="copy">
-        {'When minted, each Creep Kid is assigned a unique set of RPG attributes and their names are created using GPT-3.'}
-      </div>
-      <div id="section_how_to" className="section how-to">
-        <h2>How To</h2>
-          {'Connect and pay through Metamask or PayPal'}
-          {'Metamask: Install or send ETH to your Metamask wallet (purchasing ETH is easy through Coinbase or PayPal)'}
-          {'Paypal: TBD'}
-      </div>
-      <div id="section_roadmap" className="section roadmap">
-        <h2>Roadmap</h2>
-        {'20% Creep Kids t-shirt for all owenrs'}
-        {'30% Provenance'}
-      </div>
-      <div id="section_faq" className="section faq">
-        <h2>FAQ</h2>
-        {'TBD'}
-      </div>
-      <div id="section_about" className="section about">
-        <h2>About</h2>
-        {'TBD'}
-      </div>
-      <div id="section_help" className="section help">
-        <h2>Help</h2>
-        {'TBD'}
+    <div className={`${getContainerClass()}`}>
+      <div className="home-container">
+        <div className="logo">
+          <img src={"./assets/images/creep_kids_logo-red.gif"} alt="" />
+        </div>
+        <div className="menu">
+          <Menu />
+        </div>
+        <div className="teaser">
+          <img src={"./assets/images/NFT-TEASE-V1.gif"} alt="" />
+        </div>
+        <div id="section_buy" className="section mint">
+          <Button text={'mint'} />
+        </div>
+        <div className="copy">
+          {'Creep Kids are immortal souls minted in real time, programmatically on the Ethereum blockchain. There are 1000 unique possibilities, no two are alike. They are NFT\'s that can be traded on Opensea.'}
+        </div>
+        <div className="gallery">
+          Gallery TBD
+        </div>
+        <div className="copy">
+          {'When minted, each Creep Kid is assigned a unique set of RPG attributes and their names are created using GPT-3.'}
+        </div>
+        <div id="section_how_to" className="section how-to">
+          <h2>How To</h2>
+            <div className="copy-inner">{'Connect and pay through Metamask or PayPal'}</div>
+            <div className="copy-inner">{'Metamask: Install or send ETH to your Metamask wallet (purchasing ETH is easy through Coinbase or PayPal)'}</div>
+            <div className="copy-inner">{'Paypal: TBD'}</div>
+        </div>
+        <div id="section_roadmap" className="section roadmap">
+          <h2>Roadmap</h2>
+          <div className="copy-inner">{'20% Creep Kids t-shirt for all owenrs'}</div>
+          <div className="copy-inner">{'30% Provenance'}</div>
+        </div>
+        <div id="section_faq" className="section faq">
+          <h2>FAQ</h2>
+          {'TBD'}
+        </div>
+        <div id="section_about" className="section about">
+          <h2>About</h2>
+          {'TBD'}
+        </div>
+        <div id="section_help" className="section help">
+          <h2>Help</h2>
+          {'TBD'}
+        </div>
       </div>
     </div>
   )
